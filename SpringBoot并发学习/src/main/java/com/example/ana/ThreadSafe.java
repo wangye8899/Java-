a1a6876e2d58d0e964466215960432189f67e17c
wangye8899 <3091485316@qq.com>
2020-08-28 15:47:23 +0800

    init

---
+++ SpringBoot并发学习/src/main/java/com/example/ana/ThreadSafe.java
@@ -0,0 +1,21 @@
+package com.example.ana;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/*
+用来标记线程安全的类或者写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+//    线程安全类
+
+    String value() default "";
+
+
+}
